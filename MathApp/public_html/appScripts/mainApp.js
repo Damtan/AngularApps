f5ef8f8773d6921df0e6afd4191ed946f5487872
Damtan <dgnypowicz@wi.zut.edu.pl>
2016-05-14 14:15:04 +0200

    added initial angular scripts

---
+++ MathApp/public_html/appScripts/mainApp.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var mainApp = angular.module('mainApp',['ngRoute']);
+
+mainApp.config(['$routeProvider',
+    function($routeProvider){
+        $routeProvider
+                .when('/',{
+                    templateUrl: 'index.html'
+                });
+    }]);
